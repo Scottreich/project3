71a8134f2211c7566207a31a03cdf5b34c27cd1a
Scott Reichert <scottreichert86@gmail.com>
2017-09-22 14:12:49 -0700

    backend logic, front end returning function

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,28 @@
+///// Back end
+var results = function(userInput2) {
+var arrayAlpha = [];
+for (var index= 1; index <= "userInput2"; index +=1)
+  if (index % 3 === 0) {
+    arrayAlpha.push("Ping")
+  } else if (index % 5 === 0) {
+    arrayAlpha.push("Pong")
+  } else if (index % 15 === 0) {
+    arrayAlpha.push("Ping-Pong")
+  } else {
+    arrayAlpha.push(index);
+  }
+    return arrayAlpha;
+};
 // Front end
-
 $(document).ready(function() {
   $("#form").submit(function(event) {
     event.preventDefault();
-    var userInput= pareseInt($("input#number").val());
-
-    $("#output ul").text(results)
+    console.log(typeof results);
+    var userInput= parseInt($("input#number").val());
+    // $("#output ul").text(results)
     results.forEach(function(result) {
+      $("#output ul").append("<li>" + result + "</li>")
 
     })
   });
-});
-
-    ///// Back end
-  var results = function(userInput2){
-    var arrayAlpha = [];
-    for (var index= 1; index <= "userInput2"; index +=1)
-      if (index % 3 === 0) {
-        arrayAlpha.push("Ping-Pong")
-      } else if (index % 5 === 0)
-        return arrayAlpha
 });
