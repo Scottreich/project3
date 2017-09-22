1d16b9120cf953e6117bee57dc79a7b78baef8f7
Scott Reichert <scottreichert86@gmail.com>
2017-09-22 12:04:13 -0700

    start function for backend

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,23 @@
+// Front end
+
 $(document).ready(function() {
   $("#form").submit(function(event) {
     event.preventDefault();
+    var userInput= pareseInt($("input#number").val());
+
+    $("#output ul").text(results)
+    results.forEach(function(result) {
+
+    })
+  });
+});
+
+    ///// Back end
+  var results = function(userInput2){
+    var arrayAlpha = [];
+    for (var index= 1; index <= "userInput2"; index +=1)
+      if (index % 3 === 0) {
+        arrayAlpha.push("Ping-Pong")
+      } else if (index % 5 === 0)
+        return arrayAlpha
+});
